3541111224d1860df119cc1bb7801235af685e84
Bruno Perel <brunope@qliro.local>
2017-07-19 23:00:00 +0000

    Use Bower to handle most JS dependencies

--- application/views/headergview.php
+++ application/views/headergview.php
@@ -9,18 +9,18 @@ header("Content-Type: text/html; charset=UTF-8"); ?>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 
-	<link rel="stylesheet" type="text/css" href="<?=base_url()?>css/sunny/jquery-ui-1.10.4.custom.css" />
+	<link rel="stylesheet" type="text/css" href="<?=base_url()?>vendor/jquery-ui/themes/sunny/jquery-ui.min.css" />
 	<link rel="stylesheet" type="text/css" href="<?=base_url()?>css/edgecreator.css" />
 	<link rel="stylesheet" type="text/css" href="<?=base_url()?>css/edgecreator_wizard.css" />
 	<link rel="stylesheet" type="text/css" href="<?=base_url()?>css/edgecreator_gallery.css" />
-	<link rel="stylesheet" type="text/css" href="<?=base_url()?>css/jquery.jrac.css" />
+	<link rel="stylesheet" type="text/css" href="<?=base_url()?>vendor/jrac/jrac/style.jrac.css" />
 	<link rel="shortcut icon" href="<?=base_url()?>images/favicon.ico" />
 	
-	<script type="text/javascript" src="<?=base_url()?>js/jquery-1.10.2.js" ></script>
-	<script type="text/javascript" src="<?=base_url()?>js/jquery.serializeObject.js" ></script>
-	<script type="text/javascript" src="<?=base_url()?>js/jquery.ba-resize.js" ></script>
-	<script type="text/javascript" src="<?=base_url()?>js/jquery.jrac.js" ></script>
-	<script type="text/javascript" src="<?=base_url()?>js/jquery-ui-1.10.4.custom.js" ></script>
+	<script type="text/javascript" src="<?=base_url()?>vendor/jquery/jquery.min.js" ></script>
+	<script type="text/javascript" src="<?=base_url()?>vendor/jquery-serializeObject/jquery.serializeObject.js" ></script>
+	<script type="text/javascript" src="<?=base_url()?>vendor/jquery.ba-resize/jquery.ba-resize.min.js" ></script>
+	<script type="text/javascript" src="<?=base_url()?>vendor/jrac/jrac/jquery.jrac.js" ></script>
+	<script type="text/javascript" src="<?=base_url()?>vendor/jquery-ui/ui/minified/jquery-ui.min.js" ></script>
 	
 	<script type="text/javascript" src="<?=base_url()?>js/jquery.dataSelector.js" ></script>
 	<script type="text/javascript" src="<?=base_url()?>js/json2.js" ></script>
