0282a25dd5e4a57ab144d944e398438e8bbeaff9
Bruno Perel <brunoperel@gmail.com>
2018-12-30 13:50:06 +0100

    Simplify JS and CSS inclusion

--- application/views/headergview.php
+++ application/views/headergview.php
@@ -9,18 +9,18 @@ header("Content-Type: text/html; charset=UTF-8"); ?>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 
-	<link rel="stylesheet" type="text/css" href="<?=base_url()?>vendor/jquery-ui/themes/sunny/jquery-ui.min.css" />
-	<link rel="stylesheet" type="text/css" href="<?=base_url()?>css/edgecreator.css" />
-	<link rel="stylesheet" type="text/css" href="<?=base_url()?>css/edgecreator_wizard.css" />
-	<link rel="stylesheet" type="text/css" href="<?=base_url()?>css/edgecreator_gallery.css" />
-	<link rel="stylesheet" type="text/css" href="<?=base_url()?>vendor/jrac/jrac/style.jrac.css" />
-	<link rel="shortcut icon" href="<?=base_url()?>images/favicon.ico" />
+	<link rel="stylesheet" type="text/css" href="/vendor/jquery-ui/themes/sunny/jquery-ui.min.css" />
+	<link rel="stylesheet" type="text/css" href="/css/edgecreator.css" />
+	<link rel="stylesheet" type="text/css" href="/css/edgecreator_wizard.css" />
+	<link rel="stylesheet" type="text/css" href="/css/edgecreator_gallery.css" />
+	<link rel="stylesheet" type="text/css" href="/vendor/jrac/jrac/style.jrac.css" />
+	<link rel="shortcut icon" href="/images/favicon.ico" />
 
-	<script type="text/javascript" src="<?=base_url()?>vendor/jquery/jquery.min.js" ></script>
-	<script type="text/javascript" src="<?=base_url()?>vendor/jquery-serializeObject/jquery.serializeObject.js" ></script>
-	<script type="text/javascript" src="<?=base_url()?>vendor/jquery.ba-resize/jquery.ba-resize.min.js" ></script>
-	<script type="text/javascript" src="<?=base_url()?>vendor/jrac/jrac/jquery.jrac.js" ></script>
-	<script type="text/javascript" src="<?=base_url()?>vendor/jquery-ui/jquery-ui.js" ></script>
+	<script type="text/javascript" src="/vendor/jquery/jquery.min.js" ></script>
+	<script type="text/javascript" src="/vendor/jquery-serializeObject/jquery.serializeObject.js" ></script>
+	<script type="text/javascript" src="/vendor/jquery.ba-resize/jquery.ba-resize.min.js" ></script>
+	<script type="text/javascript" src="/vendor/jrac/jrac/jquery.jrac.js" ></script>
+	<script type="text/javascript" src="/vendor/jquery-ui/jquery-ui.js" ></script>
 
     <script src="//localhost:35729/livereload.js"></script>
 
@@ -28,22 +28,22 @@ header("Content-Type: text/html; charset=UTF-8"); ?>
 		var privilege='<?=$privilege?>';
 		var username = '<?=$user?>';
 
-		var base_url='<?=base_url()?>';
+		var base_url='/';
 		var edges_url='https://edges.ducksmanager.net/edges';
 
 		if (window.location.href.match(/user=/)) {
 			location.replace(base_url);
 		}
 	</script>
-	<script type="text/javascript" src="<?=base_url()?>js/edgecreator.js" ></script>
-	<script type="text/javascript" src="<?=base_url()?>js/edgecreator_wizard.js" ></script>
-	<script type="text/javascript" src="<?=base_url()?>js/edgecreator_wizard_imagerotate.js" ></script>
+	<script type="text/javascript" src="/js/edgecreator.js" ></script>
+	<script type="text/javascript" src="/js/edgecreator_wizard.js" ></script>
+	<script type="text/javascript" src="/js/edgecreator_wizard_imagerotate.js" ></script>
 
 	<title><?=$title?></title>
 </head>
 <body id="body" style="margin:0;padding:0">
 	<div class="ajout_etape tip2 template">
-        <img src="<?=base_url()?>images/ajouter.png" title="Ajouter une étape ici"/>
+        <img src="/images/ajouter.png" title="Ajouter une étape ici"/>
     </div>
 	<?php
 	if (!empty($erreur)) {
