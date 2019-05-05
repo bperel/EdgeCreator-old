2c0876740269262bb3eeac1ed7d20a346c02f439
Bruno Perel <brunoperel@gmail.com>
2019-05-05 22:23:45 +0200

    Load a URL instead of just a hostname from the config

--- application/helpers/dm_client.php
+++ application/helpers/dm_client.php
@@ -84,7 +84,7 @@ class DmClient
     private static function get_service_results($server, $method, $path, $parameters = [], $role = 'rawsql', $do_not_chunk = false)
     {
         $ch = curl_init();
-        $url = 'https://'.$server->ip . $server->web_root . $path;
+        $url = $server->url . $server->web_root . $path;
 
         if ($method === 'GET') {
             if (count($parameters) > 0) {
