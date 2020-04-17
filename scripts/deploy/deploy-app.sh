f3ae3b90b4001d8d840205c54e70d582333910c4
Bruno Perel <brunoperel@gmail.com>
2020-04-17 18:47:16 +0200

    Preserve ownership when moving sessions

--- scripts/deploy/deploy-app.sh
+++ scripts/deploy/deploy-app.sh
@@ -17,7 +17,7 @@ deploy() {
     docker cp ${f} ${container_name}:${webdir}_new/${f}; \
   done && \
   docker exec ${container_name} /bin/bash -c "cd ${webdir}_new && bower --allow-root install" && \
-  docker exec ${container_name} /bin/bash -c "mkdir -p ${webdir}/_sessions && cp -r ${webdir}/_sessions ${webdir}_new" && \
+  docker exec ${container_name} /bin/bash -c "mkdir -p ${webdir}/_sessions && cp -rp ${webdir}/_sessions ${webdir}_new" && \
   docker exec ${container_name} /bin/bash -c "rm -rf ${webdir} && mv ${webdir}_new ${webdir} && chmod 777 -R ${webdir}/_sessions" && \
   docker exec ${container_name} /bin/bash ${webdir}/scripts/deploy/apply-app.sh `git rev-parse HEAD` && \
   docker exec ${container_name} mkdir -m 777 -p ${webdir}/_sessions
